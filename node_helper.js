990452e8decd79a77100ae260e900d510e488e03
Jakub Zak <jakub.zak@me.com>
2020-08-15 19:16:07 +0200

    testing

--- node_helper.js
+++ node_helper.js
@@ -2,4 +2,8 @@ var NodeHelper = require('node_helper');
 var request = required('request');
 var xml2js = require('xml2js');
 
-module.export = NodeHelper.create({});
+module.export = NodeHelper.create({
+	start: function() {
+		console.log('myMirror-withings starting...');
+	}
+});
