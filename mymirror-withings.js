17a5d5fd78e58ab51fe5ac4b036d194980b5715f
Jakub Zak <jakub.zak@me.com>
2020-08-15 19:53:19 +0200

    testing

--- mymirror-withings.js
+++ mymirror-withings.js
@@ -1,8 +1,14 @@
 'use strict';
 
 Module.register("myMirror-withings", {
+	
+	init: functino() {
+		console.log("Starting myMirror-withings");
+	},
+
 	getDom: function() {
 		var div = document.createElement("div");
 		div.innerHTML("Hello World");
 		return div;
-	});
+	}
+});
