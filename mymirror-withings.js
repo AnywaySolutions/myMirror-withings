8c9d566063e9b86136204aaf16d6e81b7543d1ef
Jakub Zak <jakub.zak@me.com>
2020-08-15 19:45:41 +0200

    testing

--- mymirror-withings.js
+++ mymirror-withings.js
@@ -1,21 +1,9 @@
 'use strict';
 
 Module.register("myMirror-withings", {
-	jsonData: null,
-	errorData: null,
-
-	defaults: {
-		updateInterval: 60000,
-	}
-
 	getDom: function() {
 		var table = document.createElement("table");
-
 		table.classList.add("mymirror-withings-table");
-
 		table.innerHTML("Hello World");
-
 		return table;
-	},
-
-});
+	});
--- node_helper.js
+++
@@ -1,7 +0,0 @@
-var NodeHelper = require('node_helper');
-
-smodule.export = NodeHelper.create({
-	start: function() {
-		console.log('myMirror-withings starting...');
-	}
-});
